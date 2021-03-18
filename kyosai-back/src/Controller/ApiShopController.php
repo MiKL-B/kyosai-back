4c60734de0d3632abe9dd94c9f552d44fa87b078
Michael <michaelbecquer@yahoo.fr>
2021-03-18 16:27:56 +0100

    create SHOP PAGE, create API, get JSON

---
+++ kyosai-back/src/Controller/ApiShopController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiShopController extends AbstractController
+{
+    /**
+     * @Route("/api/shop", name="api_shop_index", methods={"GET"})
+     */
+    public function index(): response
+    {
+        $json = json_encode([
+            "text" => "ceci est un paragraphe",
+            "name" => "mike"
+        ]);
+
+
+        return  $this->json($json, 200, []);
+    }
+}
