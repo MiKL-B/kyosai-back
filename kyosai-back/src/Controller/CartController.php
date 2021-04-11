80b5b2b29a8d2a38ead67b8abf9f5e43582f18eb
Michael <michaelbecquer@yahoo.fr>
2021-04-11 12:55:25 +0200

    feat: cart wip

--- kyosai-back/src/Controller/CartController.php
+++ kyosai-back/src/Controller/CartController.php
@@ -2,9 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Produits;
 use App\Repository\ProduitsRepository;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,42 +15,48 @@ class CartController extends AbstractController
      */
     public function index(SessionInterface $session, ProduitsRepository $produitRepository)
     {
+
         $panier = $session->get('panier', []);
         $panierWithData = [];
-        foreach ($panier as $id => $quantity) {
-            $panierWithData[] = [
-                'product' => $produitRepository->find($id),
-                'quantity' => $quantity,
-            ];
-        };
         $total = 0;
-        //foreach ($panierWithData as $item) {
-        //    $totalItem = $item['product']->getPrix() * $item['quantity'];
-         //   $total += $totalItem;
-       // }
+
+        foreach ($panier as $id => $quantity) {
+
+            $produit = $produitRepository->find($id);
+            $panierWithData[] = [
+                "produit" => $produit,
+                'quantity' => $quantity
+            ];
+            $total += $produit->getPrix() * $quantity;
+        };
+
 
         return $this->json([
-            'session' => $session,
-            'items' => $panierWithData,
+            'panierWithData' => $panierWithData,
             'total' => $total
         ]);
     }
     /**
      * @Route("/panier/add/{id}", name="cart_add", methods={"POST","GET"})
      */
-    public function add($id, SessionInterface $session)
+    public function add(Produits $produit, SessionInterface $session)
     {
 
         //recuperation du panier
         $panier = $session->get('panier', []);
 
+
+        //pour qu'on puisse pas ajouter un produit avec un id qui n'existe pas
+        $id = $produit->getId();
         if (!empty($panier[$id])) {
             $panier[$id]++;
         } else {
             $panier[$id] = 1;
         }
-
         $session->set('panier', $panier);
-        return $this->json(['panier' => $panier]);
+
+
+        return $this->redirectToRoute('cart_index');
+     
     }
 }
