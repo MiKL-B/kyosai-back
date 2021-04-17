fd3840bb20a89129f3258b5033ee373f017cba4c
Michael <michaelbecquer@yahoo.fr>
2021-04-17 12:45:56 +0200

    feat: verifUser

--- kyosai-back/src/Controller/MailContactController.php
+++ kyosai-back/src/Controller/MailContactController.php
@@ -16,18 +16,18 @@ class MailContactController extends AbstractController
      */
     public function index(Request $request, MailerInterface $mailer)
     {
-        $email = (new Email())
-            ->from('hello@example.com')
-            ->to('you@example.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+        // $email = (new Email())
+        //     ->from('hello@example.com')
+        //     ->to('you@example.com')
+        //     //->cc('cc@example.com')
+        //     //->bcc('bcc@example.com')
+        //     //->replyTo('fabien@example.com')
+        //     //->priority(Email::PRIORITY_HIGH)
+        //     ->subject('Time for Symfony Mailer!')
+        //     ->text('Sending emails is fun again!')
+        //     ->html('<p>See Twig integration for better HTML integration!</p>');
 
-        $mailer->send($email);
-        return $this->json($mailer);
+        // $mailer->send($email);
+        // return $this->json($mailer);
     }
 }
